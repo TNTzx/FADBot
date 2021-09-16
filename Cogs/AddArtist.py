37edca5468ba5ab1779ab92320776f80891aee88
TheTNTBlower77 <xanderandrewritua@gmail.com>
2021-09-16 22:05:50 +0800

    new

--- Cogs/AddArtist.py
+++ Cogs/AddArtist.py
@@ -33,26 +33,60 @@ class GetArtists(commands.Cog):
             GetArtists.isUsingArtistAddCommand.append(ctx.author.id)
 
 
-        await ctx.author.send("> **The verified artist will now be set up here.**")
+        await ctx.author.send("> **The verified artist will now be set up here.**\nIf you mistyped something, you can edit it before the submission is sent, don't worry.")
         await ctx.send("You have been DM'ed. Please follow the instructions there.")
 
-        async def sendCreate(prefix, expectedType="text"):
-            response = []
+        async def sendCreate(prefix, expectedType="text", singleInput=True, skippable=False, outputAsDict=False, dictCheck="usageRights"):
+            if outputAsDict:
+                response = {}
+            else:
+                response = []
+
+            defaultValue = ""
+            if dictCheck == "usageRights":
+                defaultValue = {"All songs:", "Verified"}
+            
 
             # check if input is valid
-            async def checkInput(message, expectedType):
+            async def checkInput(message):
+                nonlocal response
                 if expectedType == "text":
                     if message.content == "":
                         await fas.sendError(ctx, "You didn't send a __text message__! Try again.", sendToAuthor=True)
                         return False
-                    response.append("text")
+
+                    if outputAsDict:
+                        textMain = []
+
+                        textSplit1 = message.content.split(": ")
+                        for i in textSplit1:
+                            textSplit2 = i.split("\n")
+                            for j in textSplit2:
+                                textMain.append(j)
+                        
+                        if dictCheck == "usageRights":
+                            validResponses = ["Verified", "Unverified"]
+                            for i in textMain[1::2]:
+                                if not (i in validResponses):
+                                    await fas.sendError(ctx, "You sent an invalid response! Make sure that \"Verified\" or \"Unverified\" is capitalized correctly. Try again.", sendToAuthor=True)
+                                    return False
+
+                        response = dict(zip(textMain[::2], textMain[1::2]))
+
+                    else:
+                        response.append(message.content)
+
                     return True
-                if expectedType == "image":
+
+                elif expectedType == "image":
                     imageExts = ["png", "jpeg", "jpg"]
                     url = []
 
                     # if input is a link
                     if not len(message.attachments) == 0:
+                        if singleInput and not len(message.attachments) == 1:
+                            await fas.sendError(ctx, "You sent more than one image! Please send only one. Try again.", sendToAuthor=True)
+
                         for i in message.attachments:
                             for image in imageExts:
                                 if i.url.endswith(image):
@@ -72,14 +106,19 @@ class GetArtists(commands.Cog):
                                 await fas.sendError(ctx, f"You didn't send an __image__ or a __link to an image__!\nMake sure the image is in the following formats: `{', '.join(imageExts)}`. Try again.", sendToAuthor=True)
                                 return False
                         url.append(message.content)
-                    response.append(url)
+                    response = url
                     return True
-
+                
+                
 
             # main loop
             success = False
             while not success:
-                await ctx.author.send(f"{prefix}\nThis command will time out in `{await fas.formatTime(timeoutDuration)}`. Use `{main.commandPrefix}cancel` to cancel making a new verified artist.")
+                if not skippable:
+                    await ctx.author.send(f"{prefix}\nThis command will time out in `{await fas.formatTime(timeoutDuration)}`. Use `{main.commandPrefix}cancel` to cancel making a new verified artist.")
+                else:
+                    await ctx.author.send(f"{prefix}\nThis command will time out in `{await fas.formatTime(timeoutDuration)}`. Use `{main.commandPrefix}cancel` to cancel making a new verified artist, or use {main.commandPrefix}skip to skip this command.")
+
                 try:
                     message : discord.Message = await main.bot.wait_for("message", check=lambda message: message.author == ctx.author, timeout=timeoutDuration)
                 except asyncio.TimeoutError:
@@ -88,24 +127,49 @@ class GetArtists(commands.Cog):
                     GetArtists.isUsingArtistAddCommand.remove(ctx.author.id)
                     raise cE.ExitFunction("Exited Function.")
                 
+                # checks for skips or cancels
                 if message.content == f"{main.commandPrefix}cancel":
-                    # cancelled
                     await ctx.author.send(f"Command cancelled.")
                     GetArtists.isUsingArtistAddCommand.remove(ctx.author.id)
                     raise cE.ExitFunction("Exited Function.")
+                if skippable and message.content == f"{main.commandPrefix}skip":
+                    await ctx.author.send(f"Command skipped, now using default values.")
+                    return defaultValue
                 
-                success = await checkInput(message, expectedType)
+                success = await checkInput(message)
 
             # returns responses
-            if expectedType == "text":
+            if singleInput and not outputAsDict:
                 return response[0]
-            elif expectedType == "image":
+            else:
                 return response
             
 
         # send stuff
-        rpContactProof = await sendCreate("\n Please send an __image (or images)__ to prove that you have contacted the artist.", "image")
-        rpArtistName = await sendCreate("\n Please send the name of the artist.", "text")
+        arData = {}
+
+        # arContactProof = await sendCreate("Please send an image (or images) to __prove that you have contacted the artist__. Image links also work.", expectedType="image", singleInput=False)
+        # arData["avatar"] = await sendCreate("Please send an image of the __artist's profile picture__.", expectedType="image")
+        # arData["name"] = await sendCreate("Please send __the name of the artist__.", expectedType="text")
+        arData["usageRights"] = await sendCreate(
+            """Please send __the usage rights for this artist__.
+            The format is as follows:
+            > <song list 1>: <Verified | Unverified>
+            > <song list 2>: <Verified | Unverified>
+            Example:
+            > Remixes: Unverified
+            > All other songs: Verified""",
+            outputAsDict=True, skippable=True
+        )
+        print(arData["usageRights"])
+
+        # ar = discord.Embed(name="Example", thumbnail=arData["avatar"], colour=discord.Colour.gold())
+        # ar.set_author(name=f"Artist data for {arData['name']}:", icon_url=arData["avatar"])
+
+        # await ctx.author.send(embed=ar)
+
+        # for i in arContactProof:
+        #     await ctx.author.send(i)
         
 
         # remove user from "using list"
