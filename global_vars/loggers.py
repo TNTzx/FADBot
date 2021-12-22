544a698c6db72f59ba1ffff7269347a1ca96efc5
TNTzx <xanderandrewritua@gmail.com>
2021-12-22 13:31:41 +0800

    new loggers

--- global_vars/loggers.py
+++ global_vars/loggers.py
@@ -9,8 +9,17 @@ import logging as log
 formatter = log.Formatter("[%(asctime)s | %(filename)s + %(funcName)s] [%(levelname)s]:   %(message)s")
 logs_path = os.path.join(os.path.split(__file__)[0], "..", "logs")
 
-if not os.path.isdir(logs_path):
-    os.mkdir(logs_path)
+def check_if_create_path(path):
+    """Checks if a path exists. If it doesn't, it gets created."""
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+check_if_create_path(logs_path)
+
+
+def form_filename(sort_num: int, filename: str, extension=".txt"):
+    """Format filenames!"""
+    return f"[{sort_num}] {filename}{extension}"
 
 
 def setup_file_handler(filename):
@@ -20,23 +29,37 @@ def setup_file_handler(filename):
 
     return handler
 
-default_handle = setup_file_handler("[0] master.txt")
+default_handle = setup_file_handler(form_filename(0, "master"))
 
-
-def setup_logger(name, is_master=True, filename="", level=log.DEBUG):
+log.basicConfig(filemode="w")
+def setup_logger(name, is_master=False, filename="", level=log.DEBUG):
     """Make a logger!"""
 
     logger = log.getLogger(name)
     logger.setLevel(level)
 
-    if is_master:
-        logger.addHandler(default_handle)
-    else:
+    logger.addHandler(default_handle)
+
+    if not is_master:
         handler = setup_file_handler(filename)
         logger.addHandler(default_handle)
         logger.addHandler(handler)
 
     return logger
 
+
+class LogPaths:
+    """Contains names for folders."""
+    databases = "databases"
+
+logger_paths = [getattr(LogPaths, x) for x in dir(LogPaths) if not x.startswith("__")]
+for logger_path in logger_paths:
+    check_if_create_path(os.path.join(logs_path, logger_path))
+
+
 log_master = setup_logger("master", is_master=True)
-log_vadb = setup_logger("aaa", filename="[1] vadb.txt")
+
+log_global_exc = setup_logger("global_exc", filename=form_filename(0, "global_exc"))
+
+log_firebase = setup_logger("firebase", filename=os.path.join(LogPaths.databases, form_filename(1, "firebase")))
+log_vadb = setup_logger("vadb", filename=os.path.join(LogPaths.databases, form_filename(1, "vadb")))
