b1ca60351e47e3d73f75bc7489fddee7f7782bf9
TNTzx <xanderandrewritua@gmail.com>
2021-12-31 21:58:34 +0800

    wait_for added with view checking

--- backend/main_library/asking/wait_for.py
+++ backend/main_library/asking/wait_for.py
@@ -6,6 +6,8 @@
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-locals
 # pylint: disable=too-many-statements
+# pylint: disable=unused-argument
+
 
 import typing as typ
 import asyncio
@@ -13,8 +15,10 @@ import nextcord as nx
 import nextcord.ext.commands as cmds
 
 import global_vars.variables as vrs
+import backend.main_library.asking.checks as w_f_ch
 import backend.exceptions.custom_exc as c_e
 import backend.exceptions.send_error as s_e
+import backend.main_library.other as m_o
 
 
 TIMEOUT = 60 * 10
@@ -27,14 +31,48 @@ async def send_error(ctx, suffix):
 async def wait_for_response(ctx: cmds.Context, timeout=TIMEOUT):
     """Wait for a message then return the response."""
     try:
-        check = lambda msg: ctx.author.id == msg.author.id and isinstance(msg.channel, nx.channel.DMChannel)
-        response: nx.Message = await vrs.global_bot.wait_for("message", check=check, timeout=timeout)
+        response: nx.Message = await vrs.global_bot.wait_for("message", check=w_f_ch.check_message(ctx), timeout=timeout)
 
     except asyncio.TimeoutError as exc:
         await s_e.send_error(ctx, "Command timed out. Please use the command again.")
         raise c_e.ExitFunction("Exited Function.") from exc
     return response
 
-async def wait_for_response_view(ctx: cmds.Context, view: typ.Type[nx.ui.View], timeout=TIMEOUT):
-    """Waits for a message then returns that message. If instead it was a view interaction, return the value of that interaction."""
+
+class ExampleView(nx.ui.View):
+    """An example view."""
+    def __init__(self):
+        super().__init__()
+        self.value = None
     
+    @nx.ui.button(label="one", style=nx.ButtonStyle.green)
+    async def button_one(self, button: nx.ui.Button, interact: nx.Interaction):
+        self.value = "one"
+        self.stop()
+
+
+class MessageViewCheck:
+    """A class containing identifiers for outputs of a message or a view."""
+    message = m_o.Unique()
+    view = m_o.Unique()
+
+async def wait_for_response_view(ctx: cmds.Context, original_message: nx.Message, view: typ.Type[nx.ui.View] | ExampleView, timeout=TIMEOUT):
+    """Waits for a message then returns that message. If instead it was a view interaction, return the value of that interaction."""
+
+    events = [
+        vrs.global_bot.wait_for("message", check=w_f_ch.check_message(ctx)),
+        vrs.global_bot.wait_for("interaction", check=w_f_ch.check_interaction(ctx, original_message))
+    ]
+
+    done, pending = await asyncio.wait(events, return_when=asyncio.FIRST_COMPLETED)
+
+    result: asyncio.Task = done.pop()
+    result = result.result()
+    for task in pending:
+        task: asyncio.Task = task
+        task.cancel()
+    
+    if isinstance(result, nx.Message):
+        return MessageViewCheck.message, result
+    elif isinstance(result, nx.Interaction):
+        return MessageViewCheck.view, view.value
