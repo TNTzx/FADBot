04917222e2d5e4863d307207397365a56b5f6581
TNTzx <xanderandrewritua@gmail.com>
2022-03-23 22:39:15 +0800

    change requests todo

--- backend/vadb_library/change_reqs/change_reqs.py
+++ backend/vadb_library/change_reqs/change_reqs.py
@@ -1,1 +1,16 @@
 """Contains logic for requests."""
+
+
+from .. import artists as art
+
+
+# TODO alrighty time to go ham, gonna need logs, accepting and denying requests, etc.
+# go ham future tent *patpatpat*
+
+class ChangeRequest():
+    """Parent class for all requests."""
+    def __init__(
+                self,
+                artist: art.Artist
+            ):
+        ...
