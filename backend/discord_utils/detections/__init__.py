92fad7c82bc87d0e5f40ed152f12f6792980999f
TNTzx <xanderandrewritua@gmail.com>
2022-04-07 22:45:55 +0800

    some rewrite stuff

--- backend/discord_utils/detections/__init__.py
+++ backend/discord_utils/detections/__init__.py
@@ -3,6 +3,7 @@
 
 from .detection import \
     wait_for_message, wait_for_message_view, wait_for_view, \
-    DetectionOutputTypes
+    DetectionOutputTypes, \
+    send_error
 
 from .detection_checks import check_interaction, check_message
