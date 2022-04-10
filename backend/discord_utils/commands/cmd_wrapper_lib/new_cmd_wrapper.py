7041731faa2aac0da6bb653dffd4431fbc0db80d
TNTzx <xanderandrewritua@gmail.com>
2022-04-10 21:08:57 +0800

    finishing cmd_cls_lib

---
+++ backend/discord_utils/commands/cmd_wrapper_lib/new_cmd_wrapper.py
@@ -0,0 +1,10 @@
+"""Contains the command wrapper."""
+
+
+from . import cmd_cls_lib as cmd_ext
+
+
+def command_wrap(
+        category: 
+        ):
+    """Registers this function as a command."""
