4397c1d0ca5fac49ae61f45e6aa85cc24947d8a0
TNTzx <xanderandrewritua@gmail.com>
2022-04-10 19:28:12 +0800

    added privilege request list

--- backend/discord_utils/commands/cmd_wrapper_lib/cmd_cls_ext/cmd_infos.py
+++ backend/discord_utils/commands/cmd_wrapper_lib/cmd_cls_ext/cmd_infos.py
@@ -1,7 +1,31 @@
 """Contains the `CmdInfo` class."""
 
 
+import nextcord.ext.commands as nx_cmds
+
+from . import privilege_reqs
+
+
+class CooldownInfo():
+    """Contains info about the cooldown."""
+    def __init__(
+            self,
+            length: int = 0,
+            type_: nx_cmds.BucketType = nx_cmds.BucketType.guild
+            ):
+        self.length = length
+        self.type_ = type_
+
+
 class CmdInfo():
     """Contains information about the command."""
-    def __init__(self):
-        
+    def __init__(
+            self,
+            description: str | None = None,
+            parameters: dict[str, str] | None = None,
+            aliases: list[str] | None = None,
+            cooldown: CooldownInfo = CooldownInfo(),
+            priv_req_list: 
+            ):
+        pass
+
