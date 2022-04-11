d96d41cd108aacd24fc9b18620c9f595413c3c34
TNTzx <xanderandrewritua@gmail.com>
2022-04-11 19:05:02 +0800

    fixed artistquery not getting data

--- backend/vadb_library/artists/artist_query.py
+++ backend/vadb_library/artists/artist_query.py
@@ -28,6 +28,6 @@ class ArtistQuery():
         return cls(
             artists = [
                 artist.Artist.vadb_from_get_json(artist_data)
-                for artist_data in response
+                for artist_data in response["data"]
             ]
         )
