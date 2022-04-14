d8d89564769026c5d2f35b2430588e098ddd9ceb
TNTzx <xanderandrewritua@gmail.com>
2022-04-14 19:29:57 +0800

    changed imports

--- backend/vadb_library/__init__.py
+++ backend/vadb_library/__init__.py
@@ -6,5 +6,6 @@ from .artist_lib import *
 from .change_reqs import *
 from .excepts import *
 from .other import *
+from .queries import BaseQuery
 
 from . import vadb_discord_utils as disc
--- backend/vadb_library/change_reqs/__init__.py
+++ backend/vadb_library/change_reqs/__init__.py
@@ -5,3 +5,4 @@ from .change_req import \
     ChangeRequest, \
     AddRequest, EditRequest
 from .req_exts import *
+from .req_query import RequestQuery
