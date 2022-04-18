f9ee91fb8a1c7d26cdb9fff9f7c03d677fe436ac
TNTzx <xanderandrewritua@gmail.com>
2022-04-19 00:25:08 +0800

    excepted failed cmds

--- cogs/cog_list/essentials/error_handling.py
+++ cogs/cog_list/essentials/error_handling.py
@@ -27,92 +27,96 @@ class CogErrorHandler(cog.RegisteredCog):
             exc: Exception | nx_cmds.CommandInvokeError
             ):
         """Called when there is an error in one of the commands."""
-        if isinstance(exc, nx_cmds.CommandOnCooldown):
-            time = ot.format_time(int(str(round(exc.retry_after, 0))[:-2]))
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = f"The command is on cooldown for `{time}` more!"
+        try:
+            if isinstance(exc, nx_cmds.CommandOnCooldown):
+                time = ot.format_time(int(str(round(exc.retry_after, 0))[:-2]))
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = f"The command is on cooldown for `{time}` more!"
                 ).send()
 
 
-        if isinstance(exc, nx_cmds.MissingRole):
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = f"You don't have the `{exc.missing_role}` role!"
-                ).send()
-            exc_utils.reset_cooldown(ctx)
-
-
-        if isinstance(exc, (nx_cmds.MissingRequiredArgument, nx_cmds.BadArgument)):
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = f"Make sure you have the correct parameters! Use `{CMD_PREFIX}help` to get help!"
-                ).send()
-            exc_utils.reset_cooldown(ctx)
-
-
-        if isinstance(exc, (
-                    nx_cmds.ExpectedClosingQuoteError,
-                    nx_cmds.InvalidEndOfQuotedStringError,
-                    nx_cmds.UnexpectedQuoteError
-                    )
-                ):
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = "Your quotation marks (`\"`) are wrong! Double-check the command if you have missing quotation marks!"
-                ).send()
-            exc_utils.reset_cooldown(ctx)
-
-
-        if isinstance(exc, nx_cmds.MissingRequiredArgument):
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = f"Make sure you have the correct parameters! Use `{global_vars.CMD_PREFIX}help` to get help!"
-                ).send()
-
-
-        if isinstance(exc, nx_cmds.NoPrivateMessage):
-            await exc_utils.SendFailedCmd(
-                error_place = exc_utils.ErrorPlace.from_context(ctx),
-                suffix = "This command is disabled in DMs!"
-                ).send()
-            exc_utils.reset_cooldown(ctx)
-
-
-        if isinstance(exc, nx_cmds.CommandInvokeError):
-            if isinstance(exc.original, exc_utils.ExitFunction):
-                return
-
-            if isinstance(exc.original, exc_utils.FailedCmd):
+            if isinstance(exc, nx_cmds.MissingRole):
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = f"You don't have the `{exc.missing_role}` role!"
+                    ).send()
                 exc_utils.reset_cooldown(ctx)
-                return
 
 
-            if hasattr(exc.original, "status"):
-                if exc.original.status == 403:
-                    error_message = f"Forbidden from sending. Code {exc.original.code}: {exc.original.text}"
-                    lgr.log_discord_forbidden.warning(error_message)
-
-
-            if isinstance(exc.original, asyncio.TimeoutError):
+            if isinstance(exc, (nx_cmds.MissingRequiredArgument, nx_cmds.BadArgument)):
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = f"Make sure you have the correct parameters! Use `{CMD_PREFIX}help` to get help!"
+                    ).send()
                 exc_utils.reset_cooldown(ctx)
-                await exc_utils.SendTimeout(
-                    error_send_info = exc_utils.ErrorPlace.from_context(ctx),
+
+
+            if isinstance(exc, (
+                        nx_cmds.ExpectedClosingQuoteError,
+                        nx_cmds.InvalidEndOfQuotedStringError,
+                        nx_cmds.UnexpectedQuoteError
+                        )
+                    ):
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = "Your quotation marks (`\"`) are wrong! Double-check the command if you have missing quotation marks!"
+                    ).send()
+                exc_utils.reset_cooldown(ctx)
+
+
+            if isinstance(exc, nx_cmds.MissingRequiredArgument):
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = f"Make sure you have the correct parameters! Use `{global_vars.CMD_PREFIX}help` to get help!"
                     ).send()
 
-            if isinstance(exc.original, nx.NotFound):
-                error_message = f"Not found. Code {exc.original.code}: {exc.original.text}"
-                lgr.log_discord_forbidden.warning(error_message)
+
+            if isinstance(exc, nx_cmds.NoPrivateMessage):
+                await exc_utils.SendFailedCmd(
+                    error_place = exc_utils.ErrorPlace.from_context(ctx),
+                    suffix = "This command is disabled in DMs!"
+                    ).send()
+                exc_utils.reset_cooldown(ctx)
+
+
+            if isinstance(exc, nx_cmds.CommandInvokeError):
+                if isinstance(exc.original, exc_utils.ExitFunction):
+                    return
+
+                if isinstance(exc.original, exc_utils.FailedCmd):
+                    exc_utils.reset_cooldown(ctx)
+                    return
+
+
+                if hasattr(exc.original, "status"):
+                    if exc.original.status == 403:
+                        error_message = f"Forbidden from sending. Code {exc.original.code}: {exc.original.text}"
+                        lgr.log_discord_forbidden.warning(error_message)
+
+
+                if isinstance(exc.original, asyncio.TimeoutError):
+                    exc_utils.reset_cooldown(ctx)
+                    await exc_utils.SendTimeout(
+                        error_send_info = exc_utils.ErrorPlace.from_context(ctx),
+                        ).send()
+
+                if isinstance(exc.original, nx.NotFound):
+                    error_message = f"Not found. Code {exc.original.code}: {exc.original.text}"
+                    lgr.log_discord_forbidden.warning(error_message)
+                    return
+
+                if isinstance(exc.original, disc_utils.UsageReqNotMet):
+                    return
+
+
+            if isinstance(exc, (
+                        nx_cmds.CommandNotFound,
+                    )
+                ):
                 return
 
-            if isinstance(exc.original, disc_utils.UsageReqNotMet):
-                return
-
-
-        if isinstance(exc, (
-                    nx_cmds.CommandNotFound,
-                )
-            ):
+        except exc_utils.FailedCmd:
             return
 
 
