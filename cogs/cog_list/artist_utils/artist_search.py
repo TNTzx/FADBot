9bbe71e91075326e5fc6f1df7d66268b265cfa62
TNTzx <xanderandrewritua@gmail.com>
2022-04-27 15:15:36 +0800

    started moving artist searches

---
+++ cogs/cog_list/artist_utils/artist_search.py
@@ -0,0 +1,12 @@
+"""Contains commands for searching for artists."""
+
+
+import nextcord as nx
+import nextcord.ext.commands as nx_cmds
+
+from ... import utils as cog
+
+
+class CogVerifyReq(cog.RegisteredCog):
+    """Contains commands for verifying artist requests."""
+
